97334e62e0b9a5cbc69dce42199b3e9a7fa946b5
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-29 09:38:07 +0200

    création index view, pdo et fichier fonctions

---
+++ view/indexView.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Immobellier</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>Immobellier</h1>
+            <p>
+                <a href="./adminForma/index.php" role="button">Admin</a>
+                <a href="./register/">Créer un compte</a>
+                <a href="./login/">Connexion</a>
+                <a href="./login/">Déconnexion</a>
+            </p>
+        </nav>
+    </header>
+    <main>
+        <div><a href="">Ajouter un bien</a></div>
+        <section>
+            <article>
+                <h2>Bien <span>loc/vente</span></h2>
+                <div><img src="" alt=""></div>
+                <p>description</p>
+                <p>Surface : m2</p>
+                <p>Nombre de pièces :</p>
+                <p class="gras">Prix :</p>
+            </article>
+        </section>
+    </main>
+
+</body>
+
+</html>
