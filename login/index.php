88a1902f6e06f6865aa9f2df6d6251d2dbb6a14d
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-29 10:17:17 +0200

    index login

---
+++ login/index.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Formulaire de connexion
+ */
+session_start();
+include '../inc/fonctions.php';
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+    $email = nettoieChamps($_POST['email']);
+    $pwd = nettoieChamps($_POST['pwd']);
+
+    if ($email) :
+        if (chercheEmail($email)) :
+            if (password_verify($pwd, chercheEmail($email)['pwd'])) :
+                $_SESSION['login'] = chercheEmail($email)['role'];
+                $_SESSION['id_utilisateur'] = chercheEmail($email)['id_utilisateur'];
+
+                if (chercheEmail($email)['role'] === 'admin') :
+                    redirectUrl('adminImmobellier/');
+                endif;
+
+                redirectUrl();
+            else :
+                $errors[] = 'Le mot de passe est invalide.';
+            endif;
+        else :
+            echo 'Votre email n\'est pas enregistré comme utilisateur de notre site.<br>';
+            echo 'Veuillez vous enregister avec <a href="../register">ce formulaire</a>';
+            exit();
+        endif;
+
+    else :
+        $errors[] = 'Votre email est manquant ou incorrect !';
+    endif;
+
+
+endif;
+
+require '../view/login/indexView.php';
